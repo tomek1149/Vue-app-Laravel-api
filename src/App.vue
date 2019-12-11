0dbbc770347cd183e5b3e76f703693246bd53d76
Tomasz Cichon <tomek1149@gmail.com>
2019-12-12 00:46:24 +0100

    added any path to our navigation

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,15 @@ export default {
   margin: 0 auto;
 }
 
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   font-size: 24px;
-}
+} */
 
 .logo {
   display: block;
