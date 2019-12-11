ed2f150a9fffb7a9d588a62ee96ad51a25232463
Tomasz Cichon <tomek1149@gmail.com>
2019-12-11 23:37:01 +0100

    changed repo and back to laravel

--- src/store/store.js
+++ src/store/store.js
@@ -9,18 +9,18 @@ export const store = new Vuex.Store({
     state: {
         filter: "all",
         todos: [
-            {
-                id: 1,
-                title: "Finish Vue Screnncast",
-                completed: false,
-                editing: false
-            },
-            {
-                id: 2,
-                title: "Take over world",
-                completed: false,
-                editing: false
-            }
+            // {
+            //     id: 1,
+            //     title: "Finish Vue Screnncast",
+            //     completed: false,
+            //     editing: false
+            // },
+            // {
+            //     id: 2,
+            //     title: "Take over world",
+            //     completed: false,
+            //     editing: false
+            // }
         ]
     },
     getters: {
