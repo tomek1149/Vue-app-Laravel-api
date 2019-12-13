98bc880aa111d0d1b524a6f23b4abdcf89591800
Tomasz Cichon <tomek1149@gmail.com>
2019-12-13 03:49:21 +0100

    end off app

---
+++ src/components/auth/Logout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch("clearTodos");
+    this.$store.dispatch("destroyToken").then(response => {
+      this.$router.push({ name: "home" });
+    });
+  }
+};
+</script>
